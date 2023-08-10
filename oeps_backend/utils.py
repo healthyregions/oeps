d1ddf5c29f599fe3c2c8ca688df666a10f774261
Adam Cox <mr.adamcox@gmail.com>
2023-08-10 10:15:23 -0500

    add .env.example, improve client creation

--- oeps_backend/utils.py
+++ oeps_backend/utils.py
@@ -1,14 +1,54 @@
 import os
-from google.cloud import bigquery
 from google.auth import impersonated_credentials
 from google.auth.transport.requests import Request
 from google.oauth2 import service_account
+from google.cloud.bigquery import (
+    Client,
+    Table,
+    SchemaField
+)
 
 import dotenv
 
 dotenv.load_dotenv()
 
 LOCAL_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data_local')
+
+
+def get_client():
+
+    project_id = os.getenv("BQ_PROJECT_ID")
+
+    credentials = get_credentials()
+
+    #create big query client
+    client = Client(project=project_id, credentials=credentials)
+
+    return client
+
+def get_credentials():
+
+    # prefer a local path to JSON credentials file.
+    json_crendentials_path = os.getenv('BQ_CREDENTIALS_FILE_PATH')
+    if json_crendentials_path:
+        credentials = service_account.Credentials.from_service_account_file(
+            json_crendentials_path,
+            scopes=["https://www.googleapis.com/auth/cloud-platform"],
+        )
+        if credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+
+    # if no local file, attempt to get all credential info from environment
+    else:
+        json_crendentials = get_credentials_info()
+        credentials = service_account.Credentials.from_service_account_info(
+            json_crendentials,
+            scopes=["https://www.googleapis.com/auth/cloud-platform"],
+        )
+        if credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+
+    return credentials
 
 def get_credentials_info():
 
@@ -25,37 +65,3 @@ def get_credentials_info():
         "client_x509_cert_url": os.getenv("BQ_CREDENTIALS_CLIENT_X509_CERT_URL"),
         "universe_domain": os.getenv("BQ_CREDENTIALS_UNIVERSE_DOMAIN"),
     }
-
-def make_credentials():
-
-    json_crendentials_path = os.getenv('BQ_CREDENTIALS_FILE_PATH')
-    if json_crendentials_path:
-        print(f'using crendential path: {json_crendentials_path}')
-        credentials = service_account.Credentials.from_service_account_file(
-            json_crendentials_path,
-            scopes=["https://www.googleapis.com/auth/cloud-platform"],
-        )
-        if credentials.expired and credentials.refresh_token:
-            credentials.refresh(Request())
-
-        #impersonating a target service account
-        target_principal = 'your-email-address-with-service-account'
-        target_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-        impersonated_creds = impersonated_credentials.Credentials(
-            source_credentials=credentials,
-            target_principal=target_principal,
-            target_scopes=target_scopes,
-        )
-
-    else:
-        print('using crendential info')
-        json_crendentials = get_credentials_info()
-
-        credentials = service_account.Credentials.from_service_account_info(
-            json_crendentials,
-            scopes=["https://www.googleapis.com/auth/cloud-platform"],
-        )
-        if credentials.expired and credentials.refresh_token:
-            credentials.refresh(Request())
-
-    return credentials
