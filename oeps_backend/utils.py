acf562ec929e0cce170d8b4ed7832c62b2205e82
Adam Cox <mr.adamcox@gmail.com>
2023-08-16 17:42:43 -0500

    create table def path variable

--- oeps_backend/utils.py
+++ oeps_backend/utils.py
@@ -13,6 +13,7 @@ import dotenv
 dotenv.load_dotenv()
 
 LOCAL_DATA_DIR = os.path.join(os.path.dirname(__file__), 'data_local')
+TABLE_DEF_DIR = os.path.join(os.path.dirname(__file__), 'table_definitions')
 
 
 def get_client():
