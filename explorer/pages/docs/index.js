5306ce093be168abd1c263d18707def0a0844373
Ashlynn Ivy Wimer <ashewimer@gmail.com>
2024-06-19 14:56:08 -0500

    removed markdown prefix

--- explorer/pages/docs/index.js
+++ explorer/pages/docs/index.js
@@ -22,7 +22,7 @@ const VariableTable = ({table, filters}) =>
       <td width="15%">{row['Variable Construct']}</td>
       <td width="25%">{row['Variable Proxy']}</td>
       <td width="15%">{row['Source']}</td>
-      <td width="15%">{row['markdownPrefix']}<a href={`docs/${row['markdown']}`}>{row['markdownText']}</a></td>
+      <td width="15%"><a href={`docs/${row['markdown']}`}>{row['markdownText']}</a></td>
       <td width="15%">{row['Spatial Scale']}</td>
     </tr> : null)}
     </tbody>
