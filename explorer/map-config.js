8f6dbbf370dc7d349d03f436b4f7f5e0371c32e3
Adam Cox <mr.adamcox@gmail.com>
2024-07-24 10:52:29 -0500

    initial update to v2 state and county data

--- explorer/map-config.js
+++ explorer/map-config.js
@@ -1,18 +1,24 @@
 import variables from './config/variables.json';
 
+// import stateData from './config/sources-v1/state.json';
+// import countyData from './config/sources-v1/county.json';
+// import zctaData from './config/sources-v1/zcta.json';
+// import tractData from './config/sources-v1/tract.json';
 import stateData from './config/sources/state.json';
 import countyData from './config/sources/county.json';
-import zctaData from './config/sources/zcta.json';
-import tractData from './config/sources/tract.json';
 
-const defaultVariable = "Opioid Mortality Rate";
-const defaultData = "US Counties";
+// const defaultVariable = "Opioid Mortality Rate";
+// const defaultData = "US Counties";
+const defaultVariable = "TotPop (Latest)";
+const defaultData = "US States";
+
+variables.sort((a, b) => a.theme.localeCompare(b.theme));
 
 const data = [
   stateData,
   countyData,
-  zctaData,
-  tractData
+  // zctaData,
+  // tractData,
 ]
 
 let style = {
