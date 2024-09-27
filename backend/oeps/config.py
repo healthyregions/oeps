3c09c988d69a6e5d1836bda742fd2a3f6f70bf28
Adam Cox <mr.adamcox@gmail.com>
2024-09-27 16:32:38 -0500

    update explorer build-configs to split CSVs

--- backend/oeps/config.py
+++ backend/oeps/config.py
@@ -4,4 +4,4 @@ CACHE_DIR = Path(__file__).parent.parent / '.cache'
 DATA_DIR = Path(__file__).parent / 'data'
 RESOURCES_DIR = DATA_DIR / 'resources'
 LOOKUPS_DIR = DATA_DIR / 'lookups'
-EXPLORER_CONFIG_DIR = Path(Path(__file__).parent.parent, 'explorer', 'config')
+EXPLORER_ROOT_DIR = Path(Path(__file__).parent.parent, 'explorer')
