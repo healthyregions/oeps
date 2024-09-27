eecdc6f8d29d201608df7eb8d6aedaf7b804fdb0
Adam Cox <mr.adamcox@gmail.com>
2024-09-27 16:38:00 -0500

    initial addition of all v2 latest data and sources

--- explorer/map-config.js
+++ explorer/map-config.js
@@ -1,20 +1,35 @@
-import variables from './config/variables.json';
+import * as colors from "./_webgeoda/utils/colors.js";
+import variablesBase from './config/variables.json';
+import variableSymbology from './config/symbology.json';
 
-// import stateData from './config/sources-v1/state.json';
-// import countyData from './config/sources-v1/county.json';
-// import zctaData from './config/sources-v1/zcta.json';
-// import tractData from './config/sources-v1/tract.json';
 import stateData from './config/sources/state.json';
 import countyData from './config/sources/county.json';
 import zctaData from './config/sources/zcta.json';
 import tractData from './config/sources/tract.json';
 
-// const defaultVariable = "Opioid Mortality Rate";
-// const defaultData = "US Counties";
-const defaultVariable = "TotPop (Latest)";
+const defaultVariable = "OdMortRtAv";
 const defaultData = "US States";
 
-variables.sort((a, b) => a.theme.localeCompare(b.theme));
+variablesBase.sort((a, b) => a.theme.localeCompare(b.theme));
+
+const defaultVariableDisplay = {
+  binning: "naturalBreaks",
+  numberOfBins: 8,
+  colorScale: "YlOrBr",
+}
+
+const addDisplayConfig = (v) => {
+  const displayConfig = variableSymbology[v.nProperty] ? variableSymbology[v.nProperty] : defaultVariableDisplay;
+  for (let [key, value] of Object.entries(displayConfig)) {
+    v[key] = value;
+  }
+  if (colors.colorbrewer[v.colorScale]) {
+    v.colorScale = colors.colorbrewer[v.colorScale][v.numberOfBins]
+  }
+  return v;
+}
+
+const variables = variablesBase.map(addDisplayConfig)
 
 const data = [
   stateData,
