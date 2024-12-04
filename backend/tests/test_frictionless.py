865779e299d972af8d0806396ce53442bd391f9f
Adam Cox <mr.adamcox@gmail.com>
2024-12-03 18:05:32 -0600

    update frictionless test

--- backend/tests/test_frictionless.py
+++ backend/tests/test_frictionless.py
@@ -2,5 +2,8 @@ from oeps.commands import frictionless_grp
 
 
 def test_create_data_package(runner):
-    result = runner.invoke(frictionless_grp, ["create-data-package"])
+    result = runner.invoke(
+        frictionless_grp,
+        ["create-data-package", "-d", ".cache/data-packages/.test-packages"],
+    )
     assert result.exit_code == 0
