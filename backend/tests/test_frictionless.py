e0a6a91b5a1bbc3730fdc8b732499f06245c85fb
Adam Cox <mr.adamcox@gmail.com>
2025-01-14 17:53:52 -0600

    update tests, small improvements

--- backend/tests/test_frictionless.py
+++ backend/tests/test_frictionless.py
@@ -10,6 +10,7 @@ def test_create_data_package(runner):
             runner.app.config["TEST_OUTPUT_DIR"],
             "--registry-path",
             runner.app.config["TEST_REGISTRY_DIR"],
+            "--no-cache",
         ],
     )
     assert result.exit_code == 0
