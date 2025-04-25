4c2b8a9263faeee0c15af82946bd894f27d9a963
Adam Cox <mr.adamcox@gmail.com>
2025-04-25 15:09:42 -0500

    remove unused config

--- backend/oeps/config.py
+++ backend/oeps/config.py
@@ -3,5 +3,4 @@ from pathlib import Path
 CACHE_DIR = Path(__file__).parent.parent / ".cache"
 DATA_DIR = Path(__file__).parent / "data"
 REGISTRY_DIR = Path(__file__).parent / "registry"
-LOOKUPS_DIR = DATA_DIR / "lookups"
 EXPLORER_ROOT_DIR = Path(Path(__file__).parent.parent, "explorer")
