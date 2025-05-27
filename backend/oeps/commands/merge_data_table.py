26532766a21cee5d2ca8dc60f4ee10304ecfbc7a
Adam Cox <mr.adamcox@gmail.com>
2025-05-27 10:48:50 -0500

    better handle validate input data

--- backend/oeps/commands/merge_data_table.py
+++ backend/oeps/commands/merge_data_table.py
@@ -36,25 +36,43 @@ from ._common_opts import (
     default=False,
     help="Continue without any prompts",
 )
+@click.option(
+    "--validate",
+    is_flag=True,
+    default=False,
+    help="Only run validation operations on the input data table",
+)
 @add_common_opts(registry_opt)
-def merge_data_table(source, geodata_source, year, force, registry_path, dry_run):
+def merge_data_table(
+    source, geodata_source, year, force, registry_path, dry_run, validate
+):
     """Merge data from an external CSV into the canonical CSVs in OEPS."""
 
     registry = Registry(registry_path)
-    if geodata_source not in registry.geodata_sources:
+    gs = registry.geodata_sources.get(geodata_source)
+    if not gs:
         raise ValueError(
             f"Invalid geodata_source name: {geodata_source}. Must be one of: {', '.join(registry.geodata_sources.keys())}"
         )
 
-    matched, missing = registry.check_input_table(source)
-    print(f"{len(matched)} columns match to variables already in the registry")
-    print(
-        f"{len(missing)} columns are not yet in the registry and will be ignored. List of unmatched columns:"
-    )
-    for i in missing:
-        print(i)
-    if len(missing) > 0 and not force:
-        c = input("continue? Y/n ")
-        if c and c.lower().startswith("n"):
-            exit()
-    registry.merge_table(source, geodata_source, year, dry_run=dry_run)
+    if validate:
+        matched, missing = registry.validate_input_table(source, gs["summary_level"])
+        print(f"{len(matched)} columns match to variables already in the registry")
+        for i in matched:
+            print(i)
+        print(
+            f"{len(missing)} columns are not yet in the registry and will be ignored. List of unmatched columns:"
+        )
+        for i in missing:
+            print(i)
+
+    else:
+        matched, missing = registry.validate_input_table(source, gs["summary_level"])
+        if len(missing) > 0 and not force:
+            c = input(
+                f"{len(missing)} fields will be ignored. continue? (run this command with --validate to learn more) Y/n "
+            )
+            if c and c.lower().startswith("n"):
+                exit()
+
+        registry.merge_table(source, geodata_source, year, dry_run=dry_run)
