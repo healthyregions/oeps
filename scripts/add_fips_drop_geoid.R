ecdd859145fee4b1961bec2a1ea4a7243a3e4be1
Ashlynn Ivy Wimer <ashewimer@gmail.com>
2025-06-17 14:18:13 -0400

    updated add fips script to also remove geoid column if present #121

---
+++ scripts/add_fips_drop_geoid.R
@@ -0,0 +1,33 @@
+### Author: Ashlynn Wimer
+### Date: 6/16/2025 
+### About: As per issue #121 on the Github, we need to add FIPS columns to all
+###   the data CSVs, and we need to also drop the (now deprecated) GEOID column.
+###   The easiest way to do this for current CSVs is a one-off script, such as
+###   this one.
+
+library(dplyr)
+library(stringr)
+library(purrr)
+
+add_fips <- function(file_name) {
+  data <- read.csv(file_name)
+
+  is_zcta <- str_detect(file_name, "zcta")
+  if (is_zcta) {
+    data <- data |>
+      mutate(ZCTA5 = as.character(substr(HEROP_ID, 6, nchar(HEROP_ID)))) |>
+      select(HEROP_ID, ZCTA5, everything())
+  } else {
+    data <- data |>
+      mutate(FIPS = as.character(substr(HEROP_ID, 6, nchar(HEROP_ID)))) |>
+      select(HEROP_ID, FIPS, everything())
+  }
+
+  if ("GEOID" %in% colnames(data)) {
+    data <- select(data, -GEOID)
+  }
+  invisible(write.csv(data, file_name, row.names = FALSE))
+}
+
+tables <- list.files("../backend/oeps/data/tables/", full.names = TRUE)
+map(tables, add_fips)
