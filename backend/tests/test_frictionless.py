9515ca4b5c5c983c6da16f9af55b79e791716998
Adam Cox <mr.adamcox@gmail.com>
2025-08-07 13:40:21 -0500

    Restructure metadata and themes / constructs / proxies via PagesCMS (#159)

--- backend/tests/test_frictionless.py
+++ backend/tests/test_frictionless.py
@@ -1,16 +1,12 @@
-from oeps.commands import frictionless_grp
-
-
 def test_create_data_package(runner):
     result = runner.invoke(
-        frictionless_grp,
-        [
+        args=[
             "create-data-package",
             "--destination",
             runner.app.config["TEST_OUTPUT_DIR"],
             "--registry-path",
             runner.app.config["TEST_REGISTRY_DIR"],
             "--no-cache",
-        ],
+        ]
     )
     assert result.exit_code == 0
