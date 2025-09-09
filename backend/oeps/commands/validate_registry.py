fe16a5f3a32085f79403ca00de79d199c3c0bd00
Adam Cox <mr.adamcox@gmail.com>
2025-09-09 12:45:59 -0500

    add and run op to sync table_sources from CSVs

--- backend/oeps/commands/validate_registry.py
+++ backend/oeps/commands/validate_registry.py
@@ -1,6 +1,7 @@
 import click
+from pathlib import Path
 
-from oeps.clients.registry import Registry
+from oeps.clients.registry import Registry, TableSource
 from ._common_opts import (
     add_common_opts,
     registry_opt,
@@ -9,8 +10,18 @@ from ._common_opts import (
 
 @click.command()
 @add_common_opts(registry_opt)
-def validate_registry(registry_path):
+@click.option(
+    "--sync-table-sources",
+    is_flag=True,
+    default=False,
+    help="Updates all variable table_sources values directly from CSV data.",
+)
+def validate_registry(registry_path: Path, sync_table_sources: bool):
     """Runs a series of validation processes against the current registry content."""
 
     registry = Registry(registry_path)
     registry.validate()
+    if sync_table_sources:
+        for ts in registry.table_sources.keys():
+            table_source = TableSource(ts, with_data=True)
+            registry.sync_variable_table_sources(table_source)
