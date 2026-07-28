d5d8929536ec7ff603434a04b67dc654166f6351
Sara Lambert <lambert8@illinois.edu>
2026-07-28 17:35:08 -0500

    fix: build warnings

--- explorer/components/layout/MainNav.js
+++ explorer/components/layout/MainNav.js
@@ -1,11 +1,7 @@
 import Link from 'next/link';
-import {Menu, MenuButton, MenuLink} from "@reach/menu-button";
 import "@reach/menu-button/styles.css";
 import styles from "./MainNav.module.css";
-import {NestedDropdown} from "mui-nested-menu";
 import {useRouter} from "next/router";
-import {MenuList} from "@mui/material";
-import {MenuItemData} from "mui-nested-menu";
 
 import {
   Menubar,
@@ -15,101 +11,10 @@ import {
   MenuPositioner,
   MenuPopup,
   MenuItem,
-  MenuSeparator,
-  MenuSubmenuRoot,
-  MenuSubmenuTrigger,
 } from './Menubar';
 
 export default function MainNav() {
   const router = useRouter();
-
-  const menuItemsData = {
-    label: 'Menu',
-    rightIcon: <>☰</>,
-    items: [
-      {
-        label: 'Home',
-        callback: (event, item) => router.push('/'),
-      },
-      {
-        label: 'About',
-        items: [
-          {
-            label: 'Teams',
-            callback: (event, item) => router.push('/about'),
-          },
-          {
-            label: 'Insights',
-            callback: (event, item) => router.push('/insights'),
-          },
-          {
-            label: 'Posts',
-            callback: (event, item) => router.push('/posts'),
-          }
-        ]
-      },
-      {
-        label: 'Explore',
-        items: [
-          {
-            label: 'Map',
-            callback: (event, item) => router.push('/map'),
-          },
-          {
-            label: 'Data',
-            callback: (event, item) => router.push('/download'),
-          },
-        ]
-      },
-      {
-        label: 'Documentation',
-        items: [
-          {
-            label: 'Metadata',
-            callback: (event, item) => router.push('/docs'),
-          },
-          {
-
-            label: 'Methodology',
-            items: [
-              {
-                label: 'Methods',
-                callback: (event, item) => router.push('/methods'),
-              },
-              // TODO: for "Data Inclusion" new page
-              // {
-              //   label: 'Data Inclusion',
-              //   callback: (event, item) => router.push('/dataInclusion'),
-              // },
-            ]
-          }
-        ]
-      },
-      {
-        label: 'Resources',
-        //leftIcon: <SaveIcon />,
-        items: [
-          {
-            label: 'Notebooks',
-            callback: (event, item) => router.push('/codeResources'),
-          },
-          {
-            label: 'Workshops',
-            callback: (event, item) => router.push('/insights'),
-          },
-          {
-            label: 'For Developers (OEPS Data Ecosystem)',
-            callback: (event, item) => window.open('https://healthyregions.github.io/oeps/', '_blank'),
-          }]
-      },
-      {
-        label: 'Contact',
-        //leftIcon: <SaveIcon />,
-        callback: (event, item) => router.push('/contact'),
-      },
-    ],
-  };
-
 
   return (
     <div className={styles.masthead} style={{ display: 'flex', justifyContent: 'space-between'}}>
@@ -117,19 +22,6 @@ export default function MainNav() {
         <Link href="/">OEPS Ecosystem</Link>
       </h3>
       <nav>
-          {/*<NestedDropdown*/}
-          {/*  menuItemsData={menuItemsData}*/}
-          {/*  MenuProps={{ elevation: 3, dir: 'rtl' }}*/}
-          {/*  ButtonProps={{*/}
-          {/*    color: 'inherit',*/}
-          {/*    sx: {*/}
-          {/*      fontSize: '13px',*/}
-          {/*      margin: 0,*/}
-          {/*      textTransform: 'none'*/}
-          {/*    },*/}
-          {/*    endIcon: <>☰</>*/}
-          {/*  }}>*/}
-          {/*</NestedDropdown>*/}
         <Menubar>
 
           <MenuRoot>
